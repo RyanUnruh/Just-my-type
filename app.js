0c449624dd30e57797a0493f5b02ebdfd0f62d4a
Ryan <ryan@Ryans-MacBook-Pro-4.local>
2020-01-31 10:32:28 -0600

    jul31.10am

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
-
-
   $('#keyboard-upper-container').hide();
 });
+
+// Sentences
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+
+
+// Highlight expected key
+let sentenceCounter = 0;
+let letterCounter = 0;
+let currentSentence = sentences[sentenceCounter];
+let currentLetter = currentSentence[letterCounter];
+$('#sentence').text(currentSentence);
+$('#target-letter').text(currentLetter);
 
 
 // Shift Key
@@ -17,40 +27,34 @@ $(document).keydown(function (e) {
 $(document).keyup(function (e) {
   $('.highlight').removeClass('highlight');
   if (e.keyCode === 16) {
-    
+
     $('#keyboard-upper-container').hide();
     $('#keyboard-lower-container').show();
   }
-}); 
+});
 
-  // keyboard highlight
+// keyboard highlight
+$(document).keypress((e) => {
+  $('#' + e.keyCode).addClass('highlight');
 
-  $(document).keypress((e) => {
-    $('#' + e.keyCode).addClass('highlight');
-    if(currentSentence.charCodeAt(l) === e.keyCode) {
-      $('#feedback').append("<span class= 'glyphicon-ok'></span>");
-    } else {
-      $('#feedback').append("<span class= 'glyphicon-remove'></span>");
-    } 
-    
-    if(currentSentence.charCodeAt(l) === e.keyCode)
-    $('#yellow-block').css('left', '+=17.5');
-    l++;
-    currentLetter = currentSentence[l];
+  $('#feedback').empty();
+
+  if (currentSentence.charCodeAt(letterCounter) === e.keyCode) {
+    letterCounter++;
+    currentLetter = currentSentence[letterCounter];
     $('#target-letter').text(currentLetter);
-  })
-
-// sentance
-let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-
-
-// Highlight axpected key
-let s = 0;
-let l = 0;
-let currentSentence = sentences[s];
-let currentLetter = currentSentence[l];
-
-$('#sentence').text(currentSentence);
-$('#target-letter').text(currentLetter);
+    $('#yellow-block').css('left', '+=17.5');
+    $('#feedback').append("<span class='glyphicon glyphicon-ok'></span>");
+  } else {
+    $('#feedback').append("<span class='glyphicon glyphicon-remove'></span>");
+  }
 
 
+  if (letterCounter < currentSentence.length) {
+    return
+  } else {
+    $('#sentence').empty();
+    sentenceCounter++
+    $('#sentence').text(currentSentence);  
+  }
+});
