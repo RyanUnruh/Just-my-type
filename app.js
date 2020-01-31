9d978c5bb9ebe4d1866d6daaa852080a399f5695
Ryan <ryan@Ryans-MacBook-Pro-4.local>
2020-01-31 14:42:11 -0600

    2pm

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ $(document).ready(function () {
 // Sentences
 let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
-
-// Highlight expected key
 let sentenceCounter = 0;
 let letterCounter = 0;
 let currentSentence = sentences[sentenceCounter];
@@ -14,8 +12,7 @@ let currentLetter = currentSentence[letterCounter];
 $('#sentence').text(currentSentence);
 $('#target-letter').text(currentLetter);
 
-
-// Shift Key
+// Hide (Camps) Keyboard
 $(document).keydown(function (e) {
   if (e.which === 16) {
 
@@ -25,7 +22,9 @@ $(document).keydown(function (e) {
 });
 
 $(document).keyup(function (e) {
-  $('.highlight').removeClass('highlight');
+
+  $('.highlight').removeClass('highlight');  // Highlight
+
   if (e.keyCode === 16) {
 
     $('#keyboard-upper-container').hide();
@@ -42,19 +41,35 @@ $(document).keypress((e) => {
   if (currentSentence.charCodeAt(letterCounter) === e.keyCode) {
     letterCounter++;
     currentLetter = currentSentence[letterCounter];
+
     $('#target-letter').text(currentLetter);
     $('#yellow-block').css('left', '+=17.5');
+
     $('#feedback').append("<span class='glyphicon glyphicon-ok'></span>");
   } else {
     $('#feedback').append("<span class='glyphicon glyphicon-remove'></span>");
   }
 
 
-  if (letterCounter < currentSentence.length) {
-    return
-  } else {
+  if (letterCounter == currentSentence.length) {
     $('#sentence').empty();
+    let currentSentence = sentences[sentenceCounter];
+    $('#sentence').text(currentSentence);
     sentenceCounter++
-    $('#sentence').text(currentSentence);  
-  }
+    $('#feedback').empty();
+  
+  } 
+
+    
+
+    
+
+    
+    
+ 
+
+  
+  
+
+
 });
